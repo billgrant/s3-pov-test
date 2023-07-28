322d6a6e48c35da8849d885c0573c2b36527e798
Bill Grant <39649812+billgrant@users.noreply.github.com>
2023-07-28 12:22:47 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+# change the module name if required
+module "s3_pov" {
+  # change org-name to your organization name
+  source  = "app.terraform.io/billgrant/s3-pov/aws"
+  version = "0.0.1"
+  # change tag names if desired
+  environment = "environment-tag"
+  name = "name-tag"
+}
